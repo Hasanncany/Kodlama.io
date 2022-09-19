823752a0eceb8fb885eb8e3ece9d7b5f6f7c2b47
hasan <hasan@HASAN>
2022-09-19 18:54:24 +0300

    sesli harfler

---
+++ sesliHarfler/src/sesliHarfler/Main.java
@@ -0,0 +1,23 @@
+package sesliHarfler;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		char harf = 'O';
+		
+		switch (harf) {
+		
+		case 'A':
+		case 'I':
+		case 'O':
+		case 'U':
+			System.out.println("KALIN SESLİ HARF");
+			break;
+		default:
+				System.out.println("İNCE SESLİ HARF");
+		}
+		
+	}
+
+}
