69da85c1f4e191a9817fb1dce2014e506aff0001
hasan <hasan@HASAN>
2022-09-19 18:56:03 +0300

    variables

---
+++ variables/src/variables/değişkenler.java
@@ -0,0 +1,17 @@
+package variables;
+
+public class değişkenler {
+
+	public static void main(String[] args) {
+		
+	int ogrenciSayisi = 12;
+	String mesaj = "Öğrenci sayısı : ";
+	
+	System.out.println(mesaj + ogrenciSayisi);
+	
+		
+		
+
+	}
+
+}
